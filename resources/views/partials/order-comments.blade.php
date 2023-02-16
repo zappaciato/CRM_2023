7567dd6c8cf23830c5d85d354659c63bcd2a9eab
zappaciato <58880515+zappaciato@users.noreply.github.com>
2023-02-16 15:07:44 +0100

    Vender files for pagination published and pagination bootstrap done ok

--- resources/views/partials/order-comments.blade.php
+++ resources/views/partials/order-comments.blade.php
@@ -24,16 +24,17 @@
                         </div>
                         @endforeach
 
-                        <div class="post-pagination">
+                        <div class="">
 
-                            <div class="pagination-no_spacing">
+                            <div class="">
 
-                                <ul class="pagination">
-                                    <li><a href="javascript:void(0);" class="prev"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-left"><polyline points="15 18 9 12 15 6"></polyline></svg></a></li>
+                                <ul class="">
+                                    {{$comments->links()}}
+                                    {{-- <li><a href="javascript:void(0);" class="prev"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-left"><polyline points="15 18 9 12 15 6"></polyline></svg></a></li>
                                     <li><a href="javascript:void(0);">1</a></li>
                                     <li><a href="javascript:void(0);" class="active">2</a></li>
                                     <li><a href="javascript:void(0);">3</a></li>
-                                    <li><a href="javascript:void(0);" class="next"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-right"><polyline points="9 18 15 12 9 6"></polyline></svg></a></li>
+                                    <li><a href="javascript:void(0);" class="next"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-right"><polyline points="9 18 15 12 9 6"></polyline></svg></a></li> --}}
                                 </ul>
 
                             </div>
